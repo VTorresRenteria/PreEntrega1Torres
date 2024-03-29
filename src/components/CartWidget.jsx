af1bd8181dbff27bfcaae0218b6414f3172a9bc3
Victor Torres <fernando011002@gmail.com>
2024-03-28 18:04:53 -0600

    incio de entrega

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+function CartWidget() {
+    
+    return (
+
+            <a href="#" className="">carrito de compras</a>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import CartWidget from "./CartWidget.jsx"
+
+function NavBar() {
+    return (
+      <header className="flex justify-around p-4 bg-red-600">
+          <h1>Valorant skins</h1>
+          <nav className="flex items-center gap-4">
+              <div className="items-center hidden md:flex md:gap-4">
+                  <a href="#">Inicio</a>
+                  <a href="#">Armas</a>
+                  <a href="#">Favoritos</a>
+              </div>
+              <CartWidget/>
+          </nav>
+      </header>
+  )
+}
+
+export default NavBar
