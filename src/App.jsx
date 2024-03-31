c07fa5990c1cdc5939cb9ba76be23940a8abf9cb
Victor Torres <fernando011002@gmail.com>
2024-03-31 14:10:46 -0600

    se agrega item list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar.jsx"
+import ItemListContainer from "./components/ItemListContainer.jsx"
 
   function App() {
 
     return (
       <>
         <NavBar/>
+        <ItemListContainer greeting="Valornt Skins"/>
       </>
     )
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,17 @@ import CartWidget from "./CartWidget.jsx"
 
 function NavBar() {
     return (
-      <header className="flex justify-around p-4 bg-red-600">
-          <h1>Valorant skins</h1>
-          <nav className="flex items-center gap-4">
-              <div className="items-center hidden md:flex md:gap-4">
-                  <a href="#">Inicio</a>
-                  <a href="#">Armas</a>
-                  <a href="#">Favoritos</a>
-              </div>
-              <CartWidget/>
-          </nav>
-      </header>
-  )
+        <header className="flex justify-around p-4 bg-red-600">
+            <h1>Valorant skins</h1>
+            <nav className="flex items-center gap-4">
+                <div className="items-center hidden md:flex md:gap-4">
+                    <a href="#">Inicio</a>
+                    <a href="#">Favoritos</a>
+                </div>
+                <CartWidget />
+            </nav>
+        </header>
+    )
 }
 
 export default NavBar
